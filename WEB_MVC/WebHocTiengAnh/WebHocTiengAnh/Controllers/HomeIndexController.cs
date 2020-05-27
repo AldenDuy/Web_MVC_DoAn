945dc5522524d5e6896300e05dcb7de0dbef3be8
Alden Duy <duybibeo@gmail.com>
2020-05-27 10:02:38 +0700

    alo

---
+++ WEB_MVC/WebHocTiengAnh/WebHocTiengAnh/Controllers/HomeIndexController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHocTiengAnh.Controllers
+{
+    public class HomeIndexController : Controller
+    {
+        // GET: HomeIndex
+        public ActionResult Home()
+        {
+            return View();
+        }
+        
+    }
+}
