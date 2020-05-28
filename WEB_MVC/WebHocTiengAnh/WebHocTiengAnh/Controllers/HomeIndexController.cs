563c81ad9189b56a165d50a674b18b4b7d370150
Alden Duy <duybibeo@gmail.com>
2020-05-28 10:48:43 +0700

    DuyUpdate

--- WEB_MVC/WebHocTiengAnh/WebHocTiengAnh/Controllers/HomeIndexController.cs
+++ WEB_MVC/WebHocTiengAnh/WebHocTiengAnh/Controllers/HomeIndexController.cs
@@ -13,6 +13,9 @@ namespace WebHocTiengAnh.Controllers
         {
             return View();
         }
-        
+        public ActionResult Course()
+        {
+            return View();
+        }
     }
 }
